8da00831c1e20d2dba105289bc110b42f79b7c68
Yaduttam Pareek <76194423+Yaduttam95@users.noreply.github.com>
2021-01-24 21:23:25 +0530

    Add files via upload

---
+++ Huge Numbers.c
@@ -0,0 +1,28 @@
+/*
+You are given two huge numbers. 
+Your job is to add them together.
+
+Input: 
+Two null terminated arrays of size 40 + the null terminator.
+
+Output: 
+A null terminated array of size 40 + the null terminator.
+
+Details: 
+The array contains numbers in ASCII 
+format making up the hudge number. 
+If the input would look like this 
+n1 = "0000000000000000000000000000001234567890" 
+and 
+n2 = "0000000000000000000000000000001234567890" 
+the output would be 
+n = "0000000000000000000000000000002469135780"
+
+Hint: 
+Tthe numbers are in ascii format and to 
+convert them to normal numbers you need 
+to do this: n[i] -= '0'; and 
+n[i] += '0'; to convert them back
+*/
+
+
